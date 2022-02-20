55ed4e970541f1c1b3cfd6d962979f4486bca40f
mwaitzman <mwaitzman@outlook.com>
2022-02-20 00:26:41 -0500

    add HW27 and HW28

---
+++ Homework/HW27/DrawingPanel.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Media;
+namespace Drawing {
+
+    internal class DrawingPanel : Panel{
+
+    static List<Line> lines;
+    static Color c = Color.FromArgb(0, 0, 0);
+
+        public DrawingPanel() {
+            this.ResizeRedraw = true;
+            this.DoubleBuffered = true;
+        }
+        protected override void OnPaint(PaintEventArgs e) {
+            base.OnPaint(e);
+            Graphics g = e.Graphics;
+            var line = lines.last();
+            g.DrawLine(new Pen(c, 2), line.x1, line.y1, line.x2, line.y2);
+        }
+        protected override void OnMouseDown (System.Windows.Forms.MouseEventArgs e) {
+            lines.add(new Line(e.X, e.Y));
+        }
+        protected override void OnMouseMove (System.Windows.Forms.MouseEventArgs e) {
+            lines.last().push(e.X, e.Y);
+            this.invalidate();
+    }
+}
+public class Line {
+    public int x1;
+    public int y1;
+    public int x2;
+    public int y2;
+
+    public Line(int x1, int y1) {
+        this.x1 = x1;
+        this.y1 = y1;
+    }
+        public void push(int x2, int y2) {
+        this.x2 = x2;
+        this.y2 = y2;
+    }
+}
+}
---
+++ Homework/HW28/OurArrayList.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GrowableArray {
+    public class OurArrayList {
+        object[] arr;
+
+        public int Count { get; private set; }
+        public OurArrayList(int initialSize) {
+            arr = new object[initialSize];
+        }
+        public OurArrayList() : this(8) {
+
+        }
+
+        public void Clear() {
+            Count = 0;
+            Array.Clear(arr);
+        }
+        public void Add(object i) {
+
+            if (Count == arr.Length) {
+                object[] temp = new object[arr.Length * 2];
+                Array.Copy(arr, temp, arr.Length);
+                arr = temp;
+                Console.WriteLine("New array size is " + arr.Length);
+            }
+            arr[Count++] = i;
+        }
+        private object Get(int index) {
+            if (index >= Count || index < 0) {
+		throw new InvalidValueException("bad value", index, arr.Length);
+            }
+            return arr[index];
+        }
+        public object this[int index] {
+            get { return Get(index); }
+            set {
+                if (index >= 0 && index < Count) {
+                    arr[index] = value;
+                }
+            }
+        }
+    }
+    class InvalidValueException : Exception {
+	int count;
+	int index;
+	public int Count {get {return count;}}
+	public int Index {get {return index;}}
+    
+	public InvalidValueException(String message, int count, int index) :  base($"{message}, {count}. {index}"){}
+    }
+    class HW28 {
+	public static void Main(string[] args) {
+	    var al = new OurArrayList(1);
+	    try {
+		_ = al[42];
+	    }
+	    catch(InvalidValueException e) {
+		Console.WriteLine(e);
+	    }
+	}
+    }
+}
