7a820d5c5879cc4488d0d9d9db7d20cda47fa010
mwaitzman <mwaitzman@outlook.com>
2022-09-04 22:45:37 -0400

    add HW71 - JS Quiz

---
+++ Homework/71/1/map.js
@@ -0,0 +1,16 @@
+"use strict";
+const map = (arr, fnMut) => {
+	let tranny_arr = [];
+	for (let idx = 0; idx < arr.length; idx++) {
+		tranny_arr[idx] = fnMut(arr[idx]);
+	}
+	return tranny_arr;
+};
+
+const test_arr = [2, 4, 6];
+const test_fnMut = item => item * 2;
+
+const output_arr = map(test_arr, test_fnMut);
+
+console.log(test_arr);
+console.log(output_arr);
